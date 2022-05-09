0e3747da06b6358f53e85b12b924950f0973bde7
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2022-05-09 12:46:56 +0900

    micropost.rbの修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,5 @@
 class Micropost < ApplicationRecord
   # 一つのmicropostsは1人のユーザーに属する
   belongs_to :user
-  validates :content, length: { maximum: 140 },
-                      presence :true
+  validates :content, length: { maximum: 140 }
 end
